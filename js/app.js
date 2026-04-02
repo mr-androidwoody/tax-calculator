85f9d0d2372f6ed9f56afc9cbb57b243a2ab73c0
mr-androidwoody <android.woody@gmail.com>
2026-04-02 19:19:48 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
+console.log('app loaded');
 
+const runBtn = document.getElementById('runCalculationBtn');
+const sampleBtn = document.getElementById('loadSampleBtn');
+const toggleBtn = document.getElementById('toggleRawJsonBtn');
+
+console.log({ runBtn, sampleBtn, toggleBtn });
+
+if (runBtn) {
+  runBtn.addEventListener('click', () => {
+    console.log('run clicked');
+  });
+}
+
+if (sampleBtn) {
+  sampleBtn.addEventListener('click', () => {
+    console.log('sample clicked');
+  });
+}
+
+if (toggleBtn) {
+  toggleBtn.addEventListener('click', () => {
+    console.log('toggle clicked');
+    const panel = document.getElementById('rawJsonPanel');
+    if (panel) panel.classList.toggle('hidden');
+  });
+}
