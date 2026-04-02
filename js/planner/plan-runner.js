ca1c96bc81b1538315c7b76c90ea5ef525d96124
mr-androidwoody <android.woody@gmail.com>
2026-04-02 18:56:31 +0100

    Add files via upload

--- js/planner/plan-runner.js
+++ js/planner/plan-runner.js
@@ -1,1 +1,19 @@
+import { calculateAnnualTax } from '../../tax/annual-tax.js';
+import { calculateHouseholdTax } from '../../tax/household-tax.js';
 
+export function runPlanner(householdInput) {
+  const peopleResults = householdInput.people.map((person) => {
+    const tax = calculateAnnualTax(person.income);
+    return {
+      ...person,
+      tax
+    };
+  });
+
+  const householdResult = calculateHouseholdTax(householdInput);
+
+  return {
+    peopleResults,
+    householdResult
+  };
+}
